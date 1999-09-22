d591c420034894b5d1058c7677a15c5f95b77f87
Paul Eggert <eggert@cs.ucla.edu>
1999-09-22 06:14:23 +0000

    (<time.h>): Remove; not used. (time): Do not declare.

--- src/mangle.c
+++ src/mangle.c
@@ -16,12 +16,6 @@
    59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include "system.h"
-
-#include <time.h>
-#ifndef time
-time_t time ();
-#endif
-
 #include "common.h"
 
 struct mangled
