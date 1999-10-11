cf4065dfe9a5fe2bf7238f58ae33d2f3365344bc
Paul Eggert <eggert@cs.ucla.edu>
1999-10-11 06:56:17 +0000

    (decode_options): Invalid dates are now treated as (time_t) -1. Redo version message to conform to GNU standards.

--- src/tar.c
+++ src/tar.c
@@ -725,7 +725,8 @@ decode_options (int argc, char *const *argv)
 
 	newer_mtime_option = get_date (optarg, 0);
 	if (newer_mtime_option == (time_t) -1)
-	  USAGE_ERROR ((0, 0, _("Invalid date format `%s'"), optarg));
+	  WARN ((0, 0, _("Substituting %s for unknown date format `%s'"),
+		 tartime (newer_mtime_option), optarg));
 
 	break;
 #endif /* not MSDOS */
@@ -1034,19 +1035,13 @@ decode_options (int argc, char *const *argv)
 
   if (show_version)
     {
-      printf ("tar (GNU %s) %s\n", PACKAGE, VERSION);
-      fputs (_("\
-\n\
-Copyright 1988, 92,93,94,95,96,97,98, 1999 Free Software Foundation, Inc.\n"),
-	     stdout);
-      fputs (_("\
-This is free software; see the source for copying conditions.  There is NO\n\
-warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"),
-	     stdout);
-      fputs (_("\
-\n\
-Written by John Gilmore and Jay Fenlason.\n"),
-	     stdout);
+      printf ("tar (GNU %s) %s\n%s\n%s\n%s\n", PACKAGE, VERSION,
+	      "Copyright 1999 Free Software Foundation, Inc.",
+	      _("\
+This program comes with NO WARRANTY, to the extent permitted by law.\n\
+You may redistribute it under the terms of the GNU General Public License;\n\
+see the file named COPYING for details."),
+	      _("Written by John Gilmore and Jay Fenlason."));
       exit (TAREXIT_SUCCESS);
     }
 
