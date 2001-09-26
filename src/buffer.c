7f1bf2e09f8ec124c0fba9d2bf033505f827353e
Paul Eggert <eggert@cs.ucla.edu>
2001-09-26 20:53:42 +0000

    just ignore them silently.

--- src/buffer.c
+++ src/buffer.c
@@ -1210,12 +1210,7 @@ flush_read (void)
 	  archive_read_error ();
 
       if (status == 0)
-	{
-	  if (left % BLOCKSIZE != 0)
-	    ERROR ((0, 0, _("%d garbage bytes ignored at end of archive"),
-		    (int) ((record_size - left) % BLOCKSIZE)));
-	  break;
-	}
+	break;
 
       if (! read_full_records_option)
 	FATAL_ERROR ((0, 0, _("Unaligned block (%lu bytes) in archive"),
