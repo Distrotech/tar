e4a052cd218d6be4d7dc6a3a6c155ea63f6dcbd5
Paul Eggert <eggert@cs.ucla.edu>
2003-07-04 23:35:57 +0000

    Convert to UTF-8.

--- doc/convtexi.pl
+++ doc/convtexi.pl
@@ -3,8 +3,24 @@ eval "exec /usr/local/bin/perl -S $0 $*"
     if 0;
 
 # Copy a Texinfo file, replacing @value's, @FIXME's and other gooddies.
-# Copyright © 1996 Free Software Foundation, Inc.
-# François Pinard <pinard@iro.umontreal.ca>, 1996.
+
+# Copyright (C) 1996, 2001 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+# FranÃ§ois Pinard <pinard@iro.umontreal.ca>, 1996.
 
 $_ = <>;
 while ($_)
