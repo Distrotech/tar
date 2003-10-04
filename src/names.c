1e7d274cd8125901600d2f302b9a108933b0c57a
Paul Eggert <eggert@cs.ucla.edu>
2003-10-04 22:30:52 +0000

    Fix some misspellings.

--- src/names.c
+++ src/names.c
@@ -636,7 +636,7 @@ names_notfound (void)
 	  ERROR ((0, 0, _("%s: Not found in archive"),
 		  quotearg_colon (cursor->name)));
 	else
-	  ERROR ((0, 0, _("%s: Required occurence not found in archive"),
+	  ERROR ((0, 0, _("%s: Required occurrence not found in archive"),
 		  quotearg_colon (cursor->name)));
       }
   
