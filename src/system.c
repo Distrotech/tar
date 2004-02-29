7fb546943ef0bd07a1bc65164e6e752b9fa4126f
Sergey Poznyakoff <gray@gnu.org.ua>
2004-02-29 10:16:02 +0000

    (sys_child_open_for_uncompress): Minor stylistic fix.

--- src/system.c
+++ src/system.c
@@ -553,7 +553,7 @@ sys_child_open_for_uncompress (void)
     {
       /* The parent tar is still here!  Just clean up.  */
 
-      read_full_records_option = 1;
+      read_full_records_option = true;
       archive = parent_pipe[PREAD];
       xclose (parent_pipe[PWRITE]);
       return child_pid;
