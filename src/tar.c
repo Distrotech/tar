2ad41b6fe05c67df09073ae8a17c9169fda2cb3d
Sergey Poznyakoff <gray@gnu.org.ua>
2005-01-05 17:30:36 +0000

    (parse_opt): Bugfix: Use ARGP_KEY_ARG. Thanks Mike Frysinger <vapier@gentoo.org> for reporting.

--- src/tar.c
+++ src/tar.c
@@ -617,9 +617,9 @@ parse_opt(int key, char *arg, struct argp_state *state)
   
   switch (key)
     {
-      case 1:
+      case ARGP_KEY_ARG:
 	/* File name or non-parsed option, because of ARGP_IN_ORDER */
-	name_add (optarg);
+	name_add (arg);
 	args->input_files++;
 	break;
 
