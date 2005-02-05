75a16f75a954f16eff5a076aa4bb51ded85e70b7
Sergey Poznyakoff <gray@gnu.org.ua>
2005-02-05 10:34:25 +0000

    (exec_error, fork_error, dup_error,pipe_error): New functions

--- src/misc.c
+++ src/misc.c
@@ -660,6 +660,30 @@ mknod_error (char const *name)
 }
 
 void
+exec_error (char const *name)
+{
+  call_arg_error ("exec", name);
+}
+
+void
+fork_error (char const *name)
+{
+  call_arg_error ("fork", name);
+}
+
+void
+dup2_error (char const *name)
+{
+  call_arg_error ("dup2", name);
+}
+
+void
+pipe_error (char const *name)
+{
+  call_arg_error ("pipe", name);
+}
+
+void
 open_error (char const *name)
 {
   call_arg_error ("open", name);
