f03fc1e078693d5315031739cf15b092349ab93c
Sergey Poznyakoff <gray@gnu.org.ua>
2005-12-09 10:05:02 +0000

    (read_directory_file): Fix format string

--- src/incremen.c
+++ src/incremen.c
@@ -454,7 +454,7 @@ read_directory_file (void)
 	incremental_version = 0;
 
       if (incremental_version > TAR_INCREMENTAL_VERSION)
-	ERROR((1, 0, _("Unsupported incremental format version: %s"),
+	ERROR((1, 0, _("Unsupported incremental format version: %d"),
 	       incremental_version));
       
       t = u = (errno = 0, strtoumax (buf, &ebuf, 10));
