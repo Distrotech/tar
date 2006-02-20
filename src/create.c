631de61428b8b840f3f87022de661f5468576bde
Sergey Poznyakoff <gray@gnu.org.ua>
2006-02-20 10:01:47 +0000

    Fix copyright years

--- src/create.c
+++ src/create.c
@@ -1,7 +1,7 @@
 /* Create a tar archive.
 
    Copyright (C) 1985, 1992, 1993, 1994, 1996, 1997, 1999, 2000, 2001,
-   2003, 2004, 2005 Free Software Foundation, Inc.
+   2003, 2004, 2005, 2006 Free Software Foundation, Inc.
 
    Written by John Gilmore, on 1985-08-25.
 
