6261c58ae80c539d4886b440eeae0ac6aec482e5
Sergey Poznyakoff <gray@gnu.org.ua>
2007-03-30 19:17:44 +0000

    (transform_name_fp): Change signature (transform_member_name): New function

--- src/common.h
+++ src/common.h
@@ -736,4 +736,5 @@ bool utf8_convert (bool to_utf, char const *input, char **output);
 /* Module transform.c */
 void set_transform_expr (const char *expr);
 bool transform_name (char **pinput);
-bool transform_name_fp (char **pinput, char *(*fun)(char *));
+bool transform_member_name (char **pinput, bool lnk);
+bool transform_name_fp (char **pinput, char *(*fun)(char *, void *), void *);
