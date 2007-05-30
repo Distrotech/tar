967fcb93181ffea221561bc09224cc6172aadbba
Sergey Poznyakoff <gray@gnu.org.ua>
2007-05-30 19:15:17 +0000

    (decx): Unknown pax keywords produce a warning,not error.

--- src/xheader.c
+++ src/xheader.c
@@ -573,8 +573,8 @@ decx (void *data, char const *keyword, char const *value, size_t size)
   if (t)
     t->decoder (st, keyword, value, size);
   else
-    ERROR((0, 0, _("Ignoring unknown extended header keyword `%s'"),
-	   keyword));
+    WARN((0, 0, _("Ignoring unknown extended header keyword `%s'"),
+	 keyword));
 }
 
 void
