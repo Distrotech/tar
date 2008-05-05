8aa729b90e579cce02d1ef77256c3c4f7b8af29c
Sergey Poznyakoff <gray@gnu.org.ua>
2008-05-05 21:33:39 +0000

    (main): Reword the "delayed error" message. New wording proposed by Karl Berry.

--- src/tar.c
+++ src/tar.c
@@ -2480,7 +2480,7 @@ main (int argc, char **argv)
   name_term ();
 
   if (exit_status == TAREXIT_FAILURE)
-    error (0, 0, _("Error exit delayed from previous errors"));
+    error (0, 0, _("Exiting with failure status due to previous errors"));
 
   if (stdlis == stdout)
     close_stdout ();
