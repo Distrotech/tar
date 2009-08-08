acd833fb9815a8fe1c2bb9b90203e008a62ee374
Sergey Poznyakoff <gray@gnu.org.ua>
2009-08-08 22:33:16 +0300

    Minor fixes

--- src/misc.c
+++ src/misc.c
@@ -762,9 +762,13 @@ dir_removed_diag (const char *name, bool top_level,
 		   void (*diagfn) (char const *name))
 {
   if (!top_level && errno == ENOENT)
-    WARNOPT (WARN_FILE_REMOVED,
-	     (0, 0, _("%s: Directory removed before we read it"),
-	      quotearg_colon (name)));
+    {
+      WARNOPT (WARN_FILE_REMOVED,
+	       (0, 0, _("%s: Directory removed before we read it"),
+		quotearg_colon (name)));
+      if (exit_status == TAREXIT_SUCCESS)
+	exit_status = TAREXIT_DIFFERS;
+    }
   else
     diagfn (name);
 }
