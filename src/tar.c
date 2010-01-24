0057cd6803a8fa322591a5cc7661d87ab77d9db6
Rob Vermaas <rob.vermaas@gmail.com>
2010-01-24 22:24:28 +0200

    Bugfix (tiny change).
    * src/tar.c (format_default_settings)[REMOTE_SHELL]: Fix
    misplaced comma.

--- src/tar.c
+++ src/tar.c
@@ -908,9 +908,9 @@ format_default_settings (void)
 	    archive_format_string (DEFAULT_ARCHIVE_FORMAT), 
 	    DEFAULT_ARCHIVE, DEFAULT_BLOCKING,              
 	    quoting_style_args[DEFAULT_QUOTING_STYLE],
-	    DEFAULT_RMT_COMMAND,
+	    DEFAULT_RMT_COMMAND
 #ifdef REMOTE_SHELL
-	    REMOTE_SHELL
+	    , REMOTE_SHELL
 #endif
 	    );
 }
