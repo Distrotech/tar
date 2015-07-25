21f86195b7c48196e7912bbb2755e56af5ea62ff
Anders Jonsson <anders.jonsson@norsjovallen.se>
2015-07-25 13:25:03 +0200

    Fix typos (preceeded etc.)
    Copyright-paperwork-exempt: yes

--- src/incremen.c
+++ src/incremen.c
@@ -1582,7 +1582,7 @@ dumpdir_ok (char *dumpdir)
 	  if (expect != 'T')
 	    {
 	      ERROR ((0, 0,
-		      _("Malformed dumpdir: 'T' not preceeded by 'R'")));
+		      _("Malformed dumpdir: 'T' not preceded by 'R'")));
 	      return false;
 	    }
 	  if (p[1] == 0 && !has_tempdir)
